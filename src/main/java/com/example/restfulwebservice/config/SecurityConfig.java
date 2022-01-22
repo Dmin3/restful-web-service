58a471b9d0e4a830545774b036a134c998eeeff8
Dmin3 <ekdrnzld@gmail.com>
2022-01-22 23:56:06 +0900

    h2-cosole 제외 메소드 추가

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -5,12 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Order(200)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //h2 console에 접속하기 위한 Springsecurity 재정의
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
     // .yml 파일보다 @Configuration이 우선순위가 더 높다!
     @Autowired
