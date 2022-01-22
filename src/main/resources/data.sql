f2597896a8fe88e1962b91cbb1180aafedf99eea
Dmin3 <ekdrnzld@gmail.com>
2022-01-22 23:55:39 +0900

    초기 data 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'User1','test111', '812131-111111');
+insert into user values (2, sysdate(), 'User2','test222', '002131-111111');
+insert into user values (3, sysdate(), 'User3','test333', '992131-111111');
