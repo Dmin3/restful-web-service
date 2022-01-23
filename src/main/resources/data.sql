40e14219fd60a49c7735ce9d7c212424fa2576c4
Dmin3 <ekdrnzld@gmail.com>
2022-01-23 14:56:50 +0900

    22/1/22

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into user values (1, sysdate(), 'User1','test111', '812131-111111');
-insert into user values (2, sysdate(), 'User2','test222', '002131-111111');
-insert into user values (3, sysdate(), 'User3','test333', '992131-111111');
+insert into user values (999, sysdate(), 'User1','test111', '812131-111111');
+insert into user values (998, sysdate(), 'User2','test222', '002131-111111');
+insert into user values (997, sysdate(), 'User3','test333', '992131-111111');
+
+insert into post values (999, 'post11', 999);
+insert into post values (1000, 'post22', 999);
